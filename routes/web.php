fc1f91fae5e564049ae1de52f739e4b93c1d00cb
DahisC <dahischeng@gmail.com>
2021-07-01 15:31:23 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function (Request $request) {
-    dump($request->all());
+    log(json_encode($request->all()));
     return view('welcome');
 });
